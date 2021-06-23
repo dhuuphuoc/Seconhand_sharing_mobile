d6ea45cd651957db34347d2e5fcb4d9aa3c441cc
phuocdh <huuphuoc97it@gmail.com>
2021-06-23 12:29:31 +0700

    fix bug

--- lib/screens/group/create_group_screen/create_group_screen.dart
+++ lib/screens/group/create_group_screen/create_group_screen.dart
@@ -43,7 +43,6 @@ class _CreateGroupScreenState extends State<CreateGroupScreen> with TickerProvid
           return NotifyDialog(S.of(context).success, S.of(context).createGroupSuccess, "OK");
         },
       ).whenComplete(() {
-        // Navigator.pop(context);
         Navigator.pushNamed(context, "/group/detail", arguments: group.id);
       });
     } else {
